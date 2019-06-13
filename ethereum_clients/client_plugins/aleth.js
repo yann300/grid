73302d7086877187366b70dd85038b9afa93da30
Everton Fraga <ev@ethereum.org>
2019-06-13 13:33:46 -0400

    fix: adding filters to Aleth

--- ethereum_clients/client_plugins/aleth.js
+++ ethereum_clients/client_plugins/aleth.js
@@ -1,8 +1,30 @@
+let platform
+
+switch (process.platform) {
+  case 'win32': {
+    platform = 'Windows'
+    break
+  }
+  case 'linux': {
+    platform = 'Linux'
+    break
+  }
+  case 'darwin': {
+    platform = 'Darwin'
+    break
+  }
+}
+
 module.exports = {
   type: 'client',
   order: 3,
   displayName: 'Aleth',
   name: 'aleth',
   repository: 'https://github.com/ethereum/aleth',
-  binaryName: 'aleth'
+  binaryName: 'aleth',
+  filter: {
+    name: {
+      includes: [platform]
+    }
+  }
 }
